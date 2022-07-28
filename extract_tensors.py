3e016a982246df011b5e02e22b1dc11ae20281f8
root <root@LAPTOP-VK1IQQGH.localdomain>
2022-07-28 10:27:25 +0100

    tensore extraction

--- extract_tensors.py
+++ extract_tensors.py
@@ -1,11 +1,18 @@
 import tensorflow as tf
+import numpy as np
 
 init_vars = tf.train.list_variables("./default_model")
 arrays = []
 names = []
 for name , shape in init_vars:
+  if("optimizer" in name):
+    continue
   print(name, shape)
+  save_name = name.replace("/","-")
   array = tf.train.load_variable("./default_model", name)
   print(array)
-  names.append(name)
-  arrays.append(array)
+  #names.append(name)
+  #arrays.append(array)
+  np.save(save_name, array)
+
+
